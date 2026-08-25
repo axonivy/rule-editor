276d4bd86fbec98f3433a8650c93a159db43fe20
ivy-dar <dominik.arnold@axonivy.com>
2026-08-25 13:30:07 +0200

    fix tests

--- playwright/tests/integration/editor.spec.ts
+++ playwright/tests/integration/editor.spec.ts
@@ -3,5 +3,5 @@ import { RuleEditor } from '../page-objects/rule-editor';
 
 test('canvas', async ({ page }) => {
   await RuleEditor.openMock(page);
-  await expect(page.getByText('rule editor')).toBeVisible();
+  await expect(page.getByText('DECISIONS')).toBeVisible();
 });
