e7872b0a57a44225d15f27b95d06a51069d19573
ivy-dar <dominik.arnold@axonivy.com>
2026-08-25 13:18:09 +0200

    fix json cliuent

--- packages/protocol/src/rule-protocol.ts
+++ packages/protocol/src/rule-protocol.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-invalid-void-type */
-import type { EditorFileContent, RuleActionArgs, RuleContext, RuleData, ValidationResult, RuleSaveData } from './data/rule';
+import type { EditorFileContent, RuleActionArgs, RuleContext, RuleEditorData, ValidationResult, RuleSaveData } from './data/rule';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface RuleMetaRequestTypes {}
 
 export interface RuleRequestTypes extends RuleMetaRequestTypes {
   initialize: [RuleContext, void];
-  data: [RuleContext, RuleData];
+  data: [RuleContext, RuleEditorData];
   saveData: [RuleSaveData, EditorFileContent];
 
   validate: [RuleContext, ValidationResult[]];
