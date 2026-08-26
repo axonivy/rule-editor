1e20a9a2226452ee3369811fca68e2568607b328
ivy-dar <dominik.arnold@axonivy.com>
2026-08-26 10:56:45 +0200

    Reactivate Main Delete Button, add translations

--- packages/editor/src/editor/main/Main.tsx
+++ packages/editor/src/editor/main/Main.tsx
@@ -1,27 +1,41 @@
 import {
   BasicField,
+  Button,
   dataTableHelper,
+  deleteFirstSelectedRow,
   Flex,
+  selectRow,
   SelectRow,
   SortableHeader,
   Table,
   TableBody,
   TableCell,
   TableGlobalFilter,
-  TableResizableHeader
+  TableResizableHeader,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  useHotkeys,
+  useTableKeyHandler,
+  type DataTableFeatures
 } from '@axonivy/ui-components';
 
 import { type Decision } from '@axonivy/rule-editor-protocol';
-import { flexRender, useTable } from '@tanstack/react-table';
+import { flexRender, useTable, type ReactTable } from '@tanstack/react-table';
 import { useAppContext } from '../../context/AppContext';
-import { useMemo } from 'react';
+import { useMemo, useEffect, useRef } from 'react';
+import { useKnownHotkeys } from '../../utils/useKnonwHotkeys';
+import { IvyIcons } from '@axonivy/ui-icons';
+import { t } from 'i18next';
 
 const { columnHelper, tableOptions } = dataTableHelper<Decision>();
 
 export const Main = () => {
-  const { data } = useAppContext();
+  const { data, setData, setSelectedIndex, detail, setDetail } = useAppContext();
 
   const decisions = useMemo(() => data?.decisions ?? [], [data]);
+
   const columns = useMemo(
     () =>
       columnHelper.columns([
@@ -43,12 +57,55 @@ export const Main = () => {
     columns
   });
 
+  useEffect(() => {
+    const subscription = table.atoms.rowSelection.subscribe(selectedRows => {
+      const selectedRowIndex = Object.keys(selectedRows).find(key => selectedRows[key]);
+      if (selectedRowIndex === undefined) {
+        setSelectedIndex(-1);
+        return;
+      }
+      setSelectedIndex(Number(selectedRowIndex));
+    });
+    return () => subscription.unsubscribe();
+  }, [table, setSelectedIndex]);
+
+  const { handleKeyDown } = useTableKeyHandler({
+    table,
+    data: decisions
+  });
+
+  const deleteRule = () =>
+    setData(old => {
+      const selectedRow = table.getSelectedRowModel().flatRows[0];
+      if (!selectedRow) {
+        return old;
+      }
+      const newData = deleteFirstSelectedRow(table, old.decisions).newData;
+      return { ...old, decisions: newData };
+    });
+
+  const resetSelection = () => {
+    selectRow(table);
+  };
+
+  const hotkeys = useKnownHotkeys();
+  const ref = useHotkeys<HTMLDivElement>(hotkeys.deleteRule.hotkey, () => deleteRule(), { scopes: ['global'] });
+  const firstElementRef = useRef<HTMLDivElement>(null);
+  useHotkeys(hotkeys.focusMain.hotkey, () => firstElementRef.current?.focus(), { scopes: ['global'] });
+
   return (
-    <Flex direction='column' className='h-full overflow-auto'>
-      <BasicField tabIndex={-1} className='m-3 min-h-0' label={'DECISIONS'} onClick={event => event.stopPropagation()}>
+    <Flex direction='column' ref={ref} onClick={resetSelection} className='h-full overflow-auto'>
+      <BasicField
+        tabIndex={-1}
+        ref={firstElementRef}
+        className='m-3 min-h-0'
+        label={t('label.editorTitle')}
+        control={<Controls table={table} deleteRule={table.getSelectedRowModel().flatRows.length > 0 ? deleteRule : undefined} />}
+        onClick={event => event.stopPropagation()}
+      >
         <TableGlobalFilter table={table} />
         <div className='overflow-x-hidden'>
-          <Table>
+          <Table onKeyDown={e => handleKeyDown(e, () => setDetail(!detail))}>
             <TableResizableHeader headerGroups={table.getHeaderGroups()} />
             <TableBody>
               {table.getRowModel().rows.map(row => (
@@ -64,6 +121,23 @@ export const Main = () => {
           </Table>
         </div>
       </BasicField>
+    </Flex>
+  );
+};
+
+const Controls = ({ table, deleteRule }: { table: ReactTable<DataTableFeatures, Decision>; deleteRule?: () => void }) => {
+  const hotkeys = useKnownHotkeys();
+  return (
+    <Flex gap={2}>
+      {/* TODO: Add "Add Rule" dialog/button here. Might simply be a button, not a full dialog? Uses the table prop*/}
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button icon={IvyIcons.Trash} onClick={deleteRule} disabled={deleteRule === undefined} aria-label={hotkeys.deleteRule.label} />
+          </TooltipTrigger>
+          <TooltipContent>{hotkeys.deleteRule.label}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </Flex>
   );
 };
